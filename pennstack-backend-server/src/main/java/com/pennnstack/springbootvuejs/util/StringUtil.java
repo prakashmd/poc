f317d20cf466c287a0102894a9ec62c5b7cc5f71
prakashmd <mdprakashit@gmail.com>
2019-10-02 12:13:31 -0400

    Corrected the pm and added test folder;

---
+++ pennstack-backend-server/src/main/java/com/pennnstack/springbootvuejs/util/StringUtil.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.pennnstack.springbootvuejs.util;
+
+/**
+ * @author pmunusamyduraikan
+ *
+ */
+public class StringUtil {
+
+	/**
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public static String append(String a,String b)
+	{
+		return a+b;
+		
+	}
+
+}
---
+++ pennstack-backend-server/src/test/java/com/pennstack/springbootvuejs/util/StringUtilTest.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.pennstack.springbootvuejs.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.pennnstack.springbootvuejs.util.StringUtil;
+
+/**
+ * @author pmunusamyduraikan
+ *
+ */
+public class StringUtilTest {
+
+	@Test
+	public void test() {
+assertEquals("ab",StringUtil.append("a", "b"));
+
+}
+
+}
